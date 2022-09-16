29e8f1330acb0d71ae075c4840e458f45d01de8a
kuchniasklep <sklep@kuchniasklep.pl>
2022-09-16 06:50:36 +0000

    Article pages and listings

--- ks-description-text.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as t,h as s}from"./index-e6122d86.js";const r=class{constructor(s){t(this,s)}render(){return s("slot",null)}};r.style="ks-description-text{display:block;margin-bottom:40px}";export{r as ks_description_text}
