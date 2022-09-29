45b02d7859e192337b069bcbb90a29e8d141eddd
kuchniasklep <sklep@kuchniasklep.pl>
2022-09-29 12:43:05 +0000

    Switch from Host to div

--- hydrate/index.js
+++ hydrate/index.js
@@ -14363,7 +14363,7 @@ class DescriptionHTML {
     registerInstance(this, hostRef);
   }
   render() {
-    return hAsync(Host, { innerHTML: this.html });
+    return hAsync("div", { innerHTML: this.html });
   }
   static get style() { return descriptionHtmlCss; }
   static get cmpMeta() { return {
