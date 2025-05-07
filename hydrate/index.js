5c567aa328faa848b55b813bfaf23966d9233c35
kuchniasklep <sklep@kuchniasklep.pl>
2025-05-07 06:53:20 +0000

    deploy-14877031007

--- hydrate/index.js
+++ hydrate/index.js
@@ -14249,7 +14249,7 @@ class CartRecycle {
   }; }
 }
 
-const cartSelectItemCss = ".items ks-cart-select-item{background-color:#252525;color:white;border-bottom:#474747 1px solid}ks-cart-select-item{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;padding:10px 7px;border-bottom:#dbdbdb 1px solid;cursor:pointer;z-index:10;position:relative;-webkit-transition:background-color 0.3s ease;transition:background-color 0.3s ease}ks-cart-select-item .name{font-size:14px}ks-cart-select-item .price{padding:0 6px;font-size:14px}@media only screen and (max-width: 420px){ks-cart-select-item .name{padding:0 5px 0 20px;font-size:14px}ks-cart-select-item .price{padding:0 10px 0 5px;font-size:14px}}";
+const cartSelectItemCss = ".items ks-cart-select-item{background-color:#252525;color:white;border-bottom:#474747 1px solid}ks-cart-select-item{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;padding:10px 7px;border-bottom:#dbdbdb 1px solid;cursor:pointer;z-index:10;position:relative;-webkit-transition:background-color 0.3s ease;transition:background-color 0.3s ease}ks-cart-select-item .name{font-size:14px;text-overflow:ellipsis;white-space:nowrap;overflow:hidden}ks-cart-select-item .price{padding:0 6px;font-size:14px;font-weight:700;white-space:nowrap}@media only screen and (max-width: 420px){ks-cart-select-item .name{padding:0 5px 0 20px;font-size:14px}ks-cart-select-item .price{padding:0 10px 0 5px;font-size:14px}}";
 
 class CartSelectItem {
   constructor(hostRef) {
@@ -14257,12 +14257,14 @@ class CartSelectItem {
     this.logo = "";
     this.name = "";
     this.price = 0;
+    this.showZero = false;
     this.color = "";
   }
   render() {
+    console.log(this.price);
     return [
       hAsync("div", { class: "name" }, this.name),
-      this.price ?
+      this.price || this.showZero ?
         hAsync("div", { class: "price" }, priceFormat(this.price))
         : null
     ];
@@ -14276,11 +14278,12 @@ class CartSelectItem {
       "logo": [513],
       "name": [513],
       "price": [514],
+      "showZero": [516, "show-zero"],
       "color": [1]
     },
     "$listeners$": undefined,
     "$lazyBundleId$": "-",
-    "$attrsToReflect$": [["logo", "logo"], ["name", "name"], ["price", "price"]]
+    "$attrsToReflect$": [["logo", "logo"], ["name", "name"], ["price", "price"], ["showZero", "show-zero"]]
   }; }
 }
 
@@ -14429,8 +14432,8 @@ class CartSelectShipping {
             this.toggled = !this.toggled;
         } }, hAsync("div", { class: "selectSlot" }, this.active == -1 ?
         hAsync("ks-cart-select-item", { name: shippingSelect }) :
-        hAsync("ks-cart-select-item", { logo: activeItem.logo, name: activeItem.name, price: activeItem.price })), hAsync("ks-icon", { name: "chevron-down" })),
-      hAsync("div", { class: "items", hidden: !this.toggled }, cart.get("shipping").map((item) => hAsync("ks-cart-select-item", { key: item.id, logo: item.logo, name: item.name, price: item.price, color: item.color, onClick: () => this.ActivateItem(item.id) })))
+        hAsync("ks-cart-select-item", { showZero: true, logo: activeItem.logo, name: activeItem.name, price: activeItem.price })), hAsync("ks-icon", { name: "chevron-down" })),
+      hAsync("div", { class: "items", hidden: !this.toggled }, cart.get("shipping").map((item) => hAsync("ks-cart-select-item", { showZero: true, key: item.id, logo: item.logo, name: item.name, price: item.price, color: item.color, onClick: () => this.ActivateItem(item.id) })))
     ];
   }
   get root() { return getElement(this); }
